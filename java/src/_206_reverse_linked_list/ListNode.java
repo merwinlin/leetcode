ae92a9583603ca67942739c23d13b5bdd9d47043
merwinlin <merwinlin@126.com>
2019-04-28 20:32:56 +0800

    206

---
+++ java/src/_206_reverse_linked_list/ListNode.java
@@ -0,0 +1,10 @@
+package _206_reverse_linked_list;
+
+/**
+ * Created by Administrator on 2019/4/28.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
